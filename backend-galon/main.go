03bd650878214ad4405311377803b10748d41f0d
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-04-30 10:33:15 +0700

    Merubah Main

--- backend-galon/main.go
+++ backend-galon/main.go
@@ -1,36 +1,73 @@
 package main
 
 import (
+	"backend-galon/database"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Product struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Price int    `json:"price"`
-}
-
-var products = []Product{
-	{ID: 1, Name: "Galon Isi Ulang", Price: 15000},
-	{ID: 2, Name: "Galon Baru", Price: 50000},
+	ID         int    `json:"id"`
+	CategoryID int    `json:"category_id"`
+	Merk       string `json:"merk"`
+	Price      int    `json:"price"`
+	Stock      int    `json:"stock"`
+	Image      string `json:"image"`
 }
 
 func main() {
+
+	// CONNECT DB
+	database.ConnectDB()
+
 	r := gin.Default()
 
 	// GET PRODUCTS
 	r.GET("/products", func(c *gin.Context) {
+
+		rows, err := database.DB.Query(`
+			SELECT id, category_id, merk, price, stock, image
+			FROM products
+		`)
+
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		var products []Product
+
+		for rows.Next() {
+
+			var product Product
+
+			rows.Scan(
+				&product.ID,
+				&product.CategoryID,
+				&product.Merk,
+				&product.Price,
+				&product.Stock,
+				&product.Image,
+			)
+
+			products = append(products, product)
+		}
+
 		c.JSON(http.StatusOK, products)
 	})
 
 	// POST ORDER
 	r.POST("/orders", func(c *gin.Context) {
+
 		var order map[string]interface{}
 
 		if err := c.ShouldBindJSON(&order); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 
