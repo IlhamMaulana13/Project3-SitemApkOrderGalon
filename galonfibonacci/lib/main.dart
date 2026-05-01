ae32782f1561e7c004e55b89b3fca0f78189f7df
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-05-01 09:37:31 +0700

    Menghubungkan Firebase

--- galonfibonacci/lib/main.dart
+++ galonfibonacci/lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:galonfibonacci/provider/cart_provider.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
--- galonfibonacci/lib/services/api_service.dart
+++ galonfibonacci/lib/services/api_service.dart
@@ -6,35 +6,23 @@ import '../models/cart_model.dart';
 import '../models/product_model.dart';
 
 class ApiService {
-
-  static const String baseUrl = "http://10.0.2.2:8080";
+  static const String baseUrl = "http://192.168.1.5:8080";
 
   // GET PRODUCTS
   static Future<List<ProductModel>> getProducts() async {
-
-    final response = await http.get(
-      Uri.parse("$baseUrl/products"),
-    );
+    final response = await http.get(Uri.parse("$baseUrl/products"));
 
     if (response.statusCode == 200) {
-
       List data = jsonDecode(response.body);
 
-      return data
-          .map((item) => ProductModel.fromJson(item))
-          .toList();
-
+      return data.map((item) => ProductModel.fromJson(item)).toList();
     } else {
       throw Exception("Failed to load products");
     }
   }
 
   // CHECKOUT
-  static Future<bool> checkout(
-    List<CartModel> cartItems,
-    int total,
-  ) async {
-
+  static Future<bool> checkout(List<CartModel> cartItems, int total) async {
     final body = {
       "user_id": "firebase_uid_123",
       "payment_method_id": 1,
@@ -51,9 +39,7 @@ class ApiService {
     final response = await http.post(
       Uri.parse("$baseUrl/orders"),
 
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: {"Content-Type": "application/json"},
 
       body: jsonEncode(body),
     );
