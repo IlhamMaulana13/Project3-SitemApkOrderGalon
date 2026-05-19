48d3b26a2e15af7597228ff8a5373514bdc183a3
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-05-19 13:21:41 +0700

    Nambahin Service

--- backend-galon/service/payment_service.go
+++ backend-galon/service/payment_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"log"
 	"os"
 
 	"github.com/midtrans/midtrans-go"
@@ -10,6 +11,10 @@ import (
 func CreatePayment(orderID string, amount int64) (*snap.Response, error) {
 
 	serverKey := os.Getenv("MIDTRANS_SERVER_KEY")
+
+	log.Println("SERVER KEY:", serverKey)
+	log.Println("ORDER ID:", orderID)
+	log.Println("AMOUNT:", amount)
 
 	var s snap.Client
 	s.New(serverKey, midtrans.Sandbox)
@@ -23,5 +28,12 @@ func CreatePayment(orderID string, amount int64) (*snap.Response, error) {
 
 	resp, err := s.CreateTransaction(req)
 
-	return resp, err
+	if err != nil {
+		log.Println("MIDTRANS CREATE ERROR:", err)
+		return nil, err
+	}
+
+	log.Println("MIDTRANS SUCCESS:", resp.RedirectURL)
+
+	return resp, nil
 }
--- galonfibonacci/lib/screens/payment_webview_screen.dart
+++ galonfibonacci/lib/screens/payment_webview_screen.dart
@@ -19,13 +19,52 @@ class _PaymentWebviewScreenState
 
   late final WebViewController controller;
 
+  bool isLoading = true;
+
   @override
   void initState() {
     super.initState();
 
     controller = WebViewController()
+
+      // AKTIFKAN JAVASCRIPT
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(widget.paymentUrl));
+
+      // BACKGROUND PUTIH
+      ..setBackgroundColor(const Color(0xFFFFFFFF))
+
+      // NAVIGATION
+      ..setNavigationDelegate(
+        NavigationDelegate(
+
+          onPageStarted: (String url) {
+            setState(() {
+              isLoading = true;
+            });
+
+            debugPrint("PAGE START: $url");
+          },
+
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+
+            debugPrint("PAGE FINISH: $url");
+          },
+
+          onWebResourceError: (WebResourceError error) {
+            debugPrint(
+              "WEBVIEW ERROR: ${error.description}",
+            );
+          },
+        ),
+      )
+
+      // LOAD URL MIDTRANS
+      ..loadRequest(
+        Uri.parse(widget.paymentUrl),
+      );
   }
 
   @override
@@ -37,9 +76,26 @@ class _PaymentWebviewScreenState
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue,
-        iconTheme: const IconThemeData(color: Colors.white),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
       ),
-      body: WebViewWidget(controller: controller),
+
+      body: Stack(
+        children: [
+
+          // WEBVIEW
+          WebViewWidget(
+            controller: controller,
+          ),
+
+          // LOADING
+          if (isLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
     );
   }
 }
